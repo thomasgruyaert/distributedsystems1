0bceb0f6f6e8872bfa9668ca1395e5c1c9ef0290
jefjacobs00 <jacobsjef16@gmail.com>
2022-10-05 15:37:50 +0200

    read and write threads

--- src/ClientReadThread.java
+++ src/ClientReadThread.java
@@ -1,2 +1,7 @@
-public class ClientReadThread {
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientReadThread  extends Thread{
+    private ChatSystemClient;
+    private Socket socket;
 }
--- src/ClientWriteThread.java
+++ src/ClientWriteThread.java
@@ -1,2 +1,14 @@
-public class ClientWriteThread {
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientWriteThread extends Thread{
+    private PrintWriter out;
+    private ChatSystemClient client;
+    private Socket socket;
+
+
+    @Override
+    public void run() {
+        super.run();
+    }
 }
