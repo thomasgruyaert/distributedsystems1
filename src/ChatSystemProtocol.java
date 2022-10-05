3d825e3df1d8f32e6de878a42d91200e3c2bf9c2
thomas1901 <49908712+thomas1901@users.noreply.github.com>
2022-10-05 14:59:10 +0200

    System protocol update

--- src/ChatSystemProtocol.java
+++ src/ChatSystemProtocol.java
@@ -1,2 +1,24 @@
+import java.util.Set;
+
 public class ChatSystemProtocol {
+    private static final int WAITING = 0;
+    private static final int ASKUSERNAME = 1;
+    private int state = WAITING;
+
+    public String processInput(String theInput, Set<String> usernames) {
+        String theOutput = null;
+
+        if (state == WAITING) {
+            theOutput = "Please enter a username";
+            state = ASKUSERNAME;
+        } else if (state == ASKUSERNAME) {
+            if (usernames.contains(theInput)){
+                theOutput = "Username already exists. Please provide a valid username.";
+            } else {
+                theOutput = "Your username is " + theInput + ". Welcome in the chat!";
+            }
+            state = WAITING;
+        }
+        return theOutput;
+    }
 }
