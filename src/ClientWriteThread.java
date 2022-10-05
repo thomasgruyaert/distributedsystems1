5a58826a425eed2bcf038e09e7057a5f9997f639
thomas1901 <49908712+thomas1901@users.noreply.github.com>
2022-10-05 15:50:29 +0200

    Updated WriteThread

--- src/ClientWriteThread.java
+++ src/ClientWriteThread.java
@@ -1,14 +1,43 @@
+import java.io.Console;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientWriteThread extends Thread{
     private PrintWriter out;
-    private ChatSystemClient client;
     private Socket socket;
+    private ChatSystemClient client;
 
+    public ClientWriteThread(Socket socket, ChatSystemClient client) {
+        this.socket = socket;
+        this.client = client;
 
-    @Override
+        try {
+            OutputStream output = socket.getOutputStream();
+            out = new PrintWriter(output, true);
+        } catch (IOException ex) {
+            System.out.println("Error getting output stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
     public void run() {
-        super.run();
+        Console console = System.console();
+        String userName = console.readLine("\nEnter your name: ");
+//        client.setUserName(userName);
+//        writer.println(userName);
+        String text;
+        do {
+            text = console.readLine("[" + "..." + "]: ");
+            out.println(text);
+        } while (!text.equals("bye"));
+
+        try {
+            socket.close();
+        } catch (IOException ex) {
+
+            System.out.println("Error writing to server: " + ex.getMessage());
+        }
     }
 }
