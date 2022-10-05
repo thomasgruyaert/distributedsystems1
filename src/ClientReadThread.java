d04977747ca4d3ad0189dca5d923d36aca541d98
thomas1901 <49908712+thomas1901@users.noreply.github.com>
2022-10-05 15:41:40 +0200

    Updated ClientReadThread

--- src/ClientReadThread.java
+++ src/ClientReadThread.java
@@ -1,7 +1,38 @@
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientReadThread  extends Thread{
-    private ChatSystemClient;
+    private BufferedReader in;
     private Socket socket;
+    private ChatSystemClient client;
+
+    public ClientReadThread(Socket socket, ChatSystemClient client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            InputStream input = socket.getInputStream();
+            in = new BufferedReader(new InputStreamReader(input));
+        } catch (IOException ex) {
+            System.out.println("Error getting input stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                String response = in.readLine();
+                System.out.println("\n" + response);
+                // prints the username after displaying the server's message
+//                if (client.getUserName() != null) {
+//                    System.out.print("[" + client.getUserName() + "]: ");
+//                }
+            } catch (IOException ex) {
+                System.out.println("Error reading from server: " + ex.getMessage());
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
 }
