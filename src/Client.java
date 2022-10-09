6f0995873715b725c9d767d52f7f86100a4fa785
Thomas Gruyaert <thomas.gruyaert@student.odisee.be>
2022-10-09 22:00:14 +0200

    Changed Project Structure

--- src/Client.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-public class Client {
-    public static void main(String[] args) throws IOException {
-        new ChatSystemClient("localhost",4200).start();
-    }
-}
--- src/Server.java
+++
@@ -1,6 +0,0 @@
-public class Server {
-    public static void main(String[] args) {
-        ChatSystemServer server = new ChatSystemServer();
-        server.Execute();
-    }
-}
