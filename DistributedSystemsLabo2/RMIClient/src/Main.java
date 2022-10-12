b9c4722929ef385ad2620f34bb39a4f5f70f25c1
jefjacobs00 <jacobsjef16@gmail.com>
2022-10-12 13:42:12 +0200

    client connection

--- DistributedSystemsLabo2/RMIClient/src/Main.java
+++ DistributedSystemsLabo2/RMIClient/src/Main.java
@@ -1,5 +1,17 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Client");
+        start();
+    }
+
+    private static void start(){
+        try {
+            Registry myRegistry = LocateRegistry.getRegistry("localhost", 1099);
+            IChatServer server = (IChatServer) myRegistry.lookup("localhost");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
